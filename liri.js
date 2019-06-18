03d6765c522d46424fd98a2a18cfd6fa89a14dc7
Binyan Lin <obinyans@gmail.com>
2019-06-18 15:27:48 -0700

    added functionality for movie query

--- liri.js
+++ liri.js
@@ -18,7 +18,6 @@ const concert = async function() {
   try {
     const response = await axios.get(`https://rest.bandsintown.com/artists/${input}/events?app_id=codingbootcamp`);
     // console.log(response.data[0]);
-    console.log(input + ' Events: \n')
     console.log('Venue Name: ' + response.data[0].venue.name);
     console.log('Venue Location: ' + response.data[0].venue.region);
     console.log('Time of Event: ' + moment(response.data[0].dateTime).format('MM/DD/YYYY'));
@@ -28,14 +27,30 @@ const concert = async function() {
 };
 // spotify-this-song
 // movie-this
+const movie = async function() {
+  try {
+    const response = await axios.get(`https://www.omdbapi.com/?t=${input}&y=&plot=short&apikey=trilogy`);
+    console.log(response.data);
+    console.log('Title: '+ response.data.Title);
+    console.log('Year: '+ response.data.Year);
+    console.log('IMDB Rating: '+ response.data.Ratings[0].Value);
+    console.log('Rotten Tomatoes Rating: '+ response.data.Ratings[1].Value);
+    console.log('Country of Production: ' + response.data.Country);
+    console.log('Language: ' + response.data.Language);
+    console.log('Plot: ' + response.data.Plot);
+    console.log('Actors: ' + response.data.Actors);
+  } catch(e) {
+    console.log(e);
+  };
+}
 // do-what-it-says
 
 if (action === 'concert-this') {
   concert();
 } if (action === 'spotify-this-song') {
-
+  
 } if (action === 'movie-this') {
-
+  movie();
 } if (action === 'do-what-it-says') {
 
 };
